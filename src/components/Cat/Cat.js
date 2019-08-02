994d277b6d74e004cdf32c6bbfe57b4f007512ce
Erin Dustin <erincdustin@gmail.com>
2019-08-02 14:54:37 -0400

    added other pets to dog and cat components

--- src/components/Cat/Cat.js
+++ src/components/Cat/Cat.js
@@ -1,22 +1,39 @@
 import React from 'react';
 
 export default function Cat(props) {
-  debugger;
   let image;
-  if(props.imageURL !== ''){
-    image = <img src={props.cat.imageURL} className="cat-image" alt={props.cat.imageDescription}></img>
+  if(props.cat[0].imageURL !== ''){
+    image = <img src={props.cat[0].imageURL} className="cat-image" alt={props.cat.imageDescription}></img>
+  }
+
+  let mappedPets;
+  if(props.cat.length > 1){
+    let currentCat = props.cat[0];
+    mappedPets = props.cat.filter(cats => cats.name !== currentCat.name).map(otherCat => {
+    return <li><img src={otherCat.imageURL} alt={otherCat.imageDescription}></img></li>}
+    );
   }
 
 return(
   <div className='cat'>
     {image}
     <section className='pet-info'>
-      <p className='pet-name'>{props.cat.name}</p>
-      <p className='pet-breed'>{props.cat.breed}</p>
-      <p className='pet-sex'>{props.cat.sex}</p>
-      <p className='pet-age'>{props.cat.age}</p>
-      <p className='pet-story'>{props.cat.story}</p>
+      <p className='pet-name'>{props.cat[0].name}</p>
+      <p className='pet-breed'>{props.cat[0].breed}</p>
+      <p className='pet-sex'>{props.cat[0].sex}</p>
+      <p className='pet-age'>{props.cat[0].age}</p>
+      <p className='pet-story'>{props.cat[0].story}</p>
     </section>
+    <h2>Additional Cats</h2>
+    <ul>
+      {mappedPets}
+    </ul>
+    {/* <button onClick={() =>{
+          await props.setGenre(e.target.value);
+          await props.getGenrePlaylist();
+          await TokenService.saveGenreToken('genre');
+          await props.history.push('/results');
+        }}</div>>Adopt Me</button> */}
   </div>
 )
 }
--- src/components/Dog/Dog.js
+++ src/components/Dog/Dog.js
@@ -1,22 +1,33 @@
 import React from 'react';
 
 export default function Dog(props) {
-
   let image;
-  if(props.imageURL !== ''){
-    image = <img src={props.dog.imageURL} className="dog-image" alt={props.dog.imageDescription}></img>
+  if(props.dog[0].imageURL !== ''){
+    image = <img src={props.dog[0].imageURL} className="dog-image" alt={props.dog.imageDescription}></img>
+  }
+
+  let mappedPets;
+  if(props.dog.length > 1){
+    let currentDog = props.dog[0];
+    mappedPets = props.dog.filter(dogs => dogs.name !== currentDog.name).map(otherdog => {
+    return <li><img src={otherdog.imageURL} alt={otherdog.imageDescription}></img></li>}
+    );
   }
 
 return(
   <div className='dog'>
     {image}
     <section className='pet-info'>
-      <p className='pet-name'>{props.dog.name}</p>
-      <p className='pet-breed'>{props.dog.breed}</p>
-      <p className='pet-sex'>{props.dog.sex}</p>
-      <p className='pet-age'>{props.dog.age}</p>
-      <p className='pet-story'>{props.dog.story}</p>
+      <p className='pet-name'>{props.dog[0].name}</p>
+      <p className='pet-breed'>{props.dog[0].breed}</p>
+      <p className='pet-sex'>{props.dog[0].sex}</p>
+      <p className='pet-age'>{props.dog[0].age}</p>
+      <p className='pet-story'>{props.dog[0].story}</p>
     </section>
+    <h2>Additional Dogs</h2>
+    <ul>
+      {mappedPets}
+    </ul>
   </div>
 )
 }
